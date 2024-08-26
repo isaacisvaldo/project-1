a05cb18eb8ea6ca8035dead440b139c126d37542
Isaac Bunga <isaac.bunga@ideiasdinamicas.com>
2024-08-26 16:36:12 +0100

    feat:update data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,25 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'project-1';
+  title = 'project- Sessão 4';
   inputText:string ='Texto Inicial'
+  buttonTitle:string='Titulo do botão'
+  buttonTitle2:string='Titulo do botão'
+  DisabledButton:boolean=true
+  DisabledButton2:boolean=false
   inputype:string='text'
   isDisable= false
+  styleObject = {width:'100px', backgroundColor:'aqua'}
+  styleObject2 = {width:'100px', backgroundColor:'red'}
 
   enableInput(){
+    this.DisabledButton2=false
+    this.DisabledButton=true
     this.isDisable= false
   }
   disableInput(){
+   this. DisabledButton2=true
+    this.DisabledButton=false
     this.isDisable= true
   }
   setTextInput(){
